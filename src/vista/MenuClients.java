d30662029edda822b7425b4ce503a07a833f1c30
josepmartorell <martorelljosep@gmail.com>
2022-03-07 03:18:53 +0100

    patch version

--- src/vista/MenuClients.java
+++ src/vista/MenuClients.java
@@ -16,7 +16,7 @@ public class MenuClients {
     
     private JFrame frame;
 
-    private JButton[] menuButtons = new JButton[4];
+    private JButton[] menuButtons = new JButton[5];
 
     private final int AMPLADA = 800;
     private final int ALCADA = 600;
@@ -31,7 +31,8 @@ public class MenuClients {
         menuButtons[0] = new JButton("0. Sortir");
         menuButtons[1] = new JButton("1. Alta Client");
         menuButtons[2] = new JButton("2. Llistar Clients");
-        menuButtons[3] = new JButton("3. Desar Client");
+        menuButtons[3] = new JButton("3. Modificar Clients");
+        menuButtons[4] = new JButton("4. Desar Client");
         
         //Addició dels botons a la finestra
         for (JButton boto : menuButtons) {
